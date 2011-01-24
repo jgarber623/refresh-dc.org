40518d46a42d3071711de560aba35b5e24fd592f
Jason Garber <jason@sixtwothree.org>
2011-01-24 14:40:27 -0500

    Removing v2 files, adding v3 files.

---
+++ javascripts/application.js
@@ -0,0 +1,25 @@
+var REFRESH = REFRESH || {};
+
+REFRESH.init = function() {
+	REFRESH.loadTweets();
+};
+
+REFRESH.loadTweets = function() {
+	var $container = $( "section.tweets" ),
+		out = "";
+
+	$.ajax({
+		dataType: "json",
+		success: function( data ) {
+			for ( i = 0; i < data.length; i++ ) {
+				out += '<li class="hentry"><p class="entry-content"><a href="http://twitter.com/refreshdc/status/' + data[i].id + '" rel="bookmark">' + data[i].text + '</a></p></li>';
+			}
+
+			$container.append( '<ol class="hfeed">' + out + '</ol>' ).fadeIn( "slow" );
+		},
+		type: "get",
+		url: "http://twitter.com/status/user_timeline/refreshdc.json?count=3&callback=?"
+	});
+};
+
+$().ready( REFRESH.init );
