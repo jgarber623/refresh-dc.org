e203bac9e434f246a3809ed313d3c9ce47c37b0c
Jason Garber <jason@sixtwothree.org>
2013-11-27 11:36:22 -0500

    Clean up Compass config. Include Breakpoint.

--- config/compass.rb
+++ config/compass.rb
@@ -1,18 +1,11 @@
-# Require any additional compass plugins here.
+require 'breakpoint'
 require 'sass-globbing'
 
-# Set this to the root of your project when deployed:
-http_path = '/'
-css_dir = 'src/jekyll/stylesheets'
-sass_dir = 'src/sass'
-images_dir = 'src/jekyll/images'
+http_path       = '/'
 
-# You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
-output_style = :compressed
+css_dir         = 'src/jekyll/stylesheets'
+images_dir      = 'src/jekyll/images'
+sass_dir        = 'src/sass'
 
-# To enable relative paths to assets via compass helper functions. Uncomment:
+output_style    = :compressed # :expanded or :nested or :compact or :compressed
 relative_assets = true
-
-# To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
