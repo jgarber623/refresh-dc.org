0c4a3fa3a5c95abd7f8babfc140e0aead8a88748
Jason Garber <jason@sixtwothree.org>
2018-03-25 14:25:15 -0400

    Remove Capistrano configuration

--- config/deploy.rb
+++
@@ -1,8 +0,0 @@
-# config valid only for current version of Capistrano
-lock '3.10.1'
-
-set :application, 'refresh-dc.org'
-set :repo_url, 'git@github.com:jgarber623/refresh-dc.org.git'
-set :chruby_ruby, 'ruby-2.4.2'
-
-before 'deploy:publishing', :build
