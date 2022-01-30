4edc2b0090978622899fa7294aae0f6121f71ea4
Jason Garber <jason@sixtwothree.org>
2022-01-29 21:54:28 -0500

    Use eleventyConfig per convention

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,19 @@
-module.exports = eleventy => {
-  eleventy.setBrowserSyncConfig(require('@jgarber/browsersync-config/eleventy'));
+module.exports = eleventyConfig => {
+  eleventyConfig.setBrowserSyncConfig(require('@jgarber/browsersync-config/eleventy'));
 
-  eleventy.setLibrary('md', require('./lib/libraries/markdown.js'));
+  eleventyConfig.setLibrary('md', require('./lib/libraries/markdown.js'));
 
-  eleventy.addDataExtension('yml', require('./lib/extensions/yaml.js'));
+  eleventyConfig.addDataExtension('yml', require('./lib/extensions/yaml.js'));
 
-  eleventy.addFilter('datetime_to_iso_string', require('./lib/filters/datetime_to_iso_string.js'));
-  eleventy.addFilter('datetime_to_string', require('./lib/filters/datetime_to_string.js'));
-  eleventy.addFilter('prettify', require('./lib/filters/prettify.js'));
+  eleventyConfig.addFilter('datetime_to_iso_string', require('./lib/filters/datetime_to_iso_string.js'));
+  eleventyConfig.addFilter('datetime_to_string', require('./lib/filters/datetime_to_string.js'));
+  eleventyConfig.addFilter('prettify', require('./lib/filters/prettify.js'));
 
-  eleventy.addPassthroughCopy('./src/assets/images');
-  eleventy.addPassthroughCopy('./src/favicon.ico');
-  eleventy.addPassthroughCopy('./src/robots.txt');
+  eleventyConfig.addPassthroughCopy('./src/assets/images');
+  eleventyConfig.addPassthroughCopy('./src/favicon.ico');
+  eleventyConfig.addPassthroughCopy('./src/robots.txt');
 
-  eleventy.addWatchTarget('./src/assets/stylesheets');
+  eleventyConfig.addWatchTarget('./src/assets/stylesheets');
 
   return {
     dir: {
