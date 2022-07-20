21ffe74108ceafd86d9d50dc6b97cfa764314055
Jason Garber <jason@sixtwothree.org>
2022-07-19 22:25:45 -0400

    Refactor lib files and Eleventy config

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,9 @@
 module.exports = eleventyConfig => {
-  eleventyConfig.setBrowserSyncConfig(require('@jgarber/browsersync-config/eleventy'));
-
   eleventyConfig.setLibrary('md', require('./lib/libraries/markdown.js'));
+  eleventyConfig.setLiquidOptions(require('./lib/libraries/liquid.js'));
 
   eleventyConfig.addDataExtension('yml', require('./lib/extensions/yaml.js'));
-
   eleventyConfig.addExtension('css', require('./lib/extensions/css.js'));
-
-  eleventyConfig.addFilter('datetime_to_iso_string', require('./lib/filters/datetime_to_iso_string.js'));
-  eleventyConfig.addFilter('datetime_to_string', require('./lib/filters/datetime_to_string.js'));
-  eleventyConfig.addFilter('prettify', require('./lib/filters/prettify.js'));
 
   eleventyConfig.addPassthroughCopy('./src/assets/images');
   eleventyConfig.addPassthroughCopy('./src/favicon.ico');
