79dfff1724927c6a2472b8227f4e866d993fa63c
Jason Garber <jason@sixtwothree.org>
2023-01-06 09:23:02 -0500

    Remove js-yaml dependency and related config

--- .eleventy.js
+++ .eleventy.js
@@ -2,8 +2,6 @@ module.exports = eleventyConfig => {
   eleventyConfig.setLibrary('md', require('./lib/libraries/markdown.js'));
   eleventyConfig.setLiquidOptions(require('./lib/libraries/liquid.js'));
 
-  eleventyConfig.addDataExtension('yml', require('./lib/extensions/yaml.js'));
-
   eleventyConfig.addPlugin(require('./lib/plugins/postcss.js'));
 
   eleventyConfig.addPassthroughCopy('./src/assets/images');
