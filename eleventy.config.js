9993036ec3bb8b62318e85a7152c8fc101b508ea
Jason Garber <jason@sixtwothree.org>
2023-07-17 21:49:08 -0400

    Simplify Eleventy config

--- eleventy.config.js
+++ eleventy.config.js
@@ -19,9 +19,7 @@ module.exports = function(eleventyConfig) {
 
   return {
     dir: {
-      input: './src',
-      layouts: '_layouts',
-      output: './public'
+      input: './src'
     }
   };
 };
